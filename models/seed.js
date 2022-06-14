b963710a172981d800878ddc8b266e2457ab89bd
victortan <vtan8090@gmail.com>
2022-06-14 13:16:29 -0400

    Added comments

--- models/seed.js
+++ models/seed.js
@@ -5,28 +5,32 @@ const newsapi = new NewsAPI('1372cfdb6df94089bdb9ceb7f8b5ae9c');
 
 const db = mongoose.connection
 
+// Create a function that adds the articles
 const addBabyFormula = () => {
+    // Fetches all articles from a query 
     newsapi.v2.everything({
         sources: 'cnn',
         q: 'baby formula',
         language: 'en',
     })
+    // Take the information from the query
     .then(res => {
+    // Set a variable for the articles obtained
     let babyFormulaArticles = res.articles
+    // Mapping all of the articles to a new array with an issue property added
     let taggedFormulaArticles = babyFormulaArticles.map((article) => {
         article.issue = 'Children and Family'
         return article
     })
+    // Prep the database by clearing the data
     Issue.deleteMany({}).then((data) => {
-        // Seed Starter Fruits
+        // Insert the articles
         Issue.insertMany(taggedFormulaArticles).then((data) => {
                 console.log(data)
             })
         })
     })
 }
-
-
 
 const addClimate = () => {
     newsapi.v2.everything({
@@ -48,7 +52,7 @@ const addClimate = () => {
 
 
 
-
+// On open, add all the articles to the database
 db.on("open", () => {
     addBabyFormula()
     addClimate()
