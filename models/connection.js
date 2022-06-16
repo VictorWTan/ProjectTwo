317e77e695c6d6a96294f9a913965fd3b62b635b
victortan <vtan8090@gmail.com>
2022-06-16 13:02:19 -0400

    Add

--- models/connection.js
+++ models/connection.js
@@ -3,18 +3,18 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 // Set up database connection
-const DATABASE_URL = process.env.DATABASE_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 const CONFIG = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
 
 // Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
+mongoose.connect(MONGODB_URI, CONFIG);
 
 // When the connection opens, closes, or errors, log them
 mongoose.connection
-.on("open", () => console.log("Connected to Mongoose"))
+.on("open", () => console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`))
 .on("close", () => console.log("Disconnected from Mongoose"))
 .on("error", (error) => console.log(error));
 
