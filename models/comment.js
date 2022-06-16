e345f3a1ba4afb3e7f0d6753ed65440ad50c0e56
victortan <vtan8090@gmail.com>
2022-06-16 09:43:20 -0400

    Added comment section

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,7 @@ const {Schema, model} = mongoose
 const commentsSchema = new Schema({
     username: {type: Schema.Types.ObjectId, ref: 'User'},
     body: String,
+    issue: {type: Schema.Types.ObjectId}, ref: 'Issue',
     createdOn: {type: Date, default:Date.now},
     updatedOn: {type: Date, default:Date.now}
 })
